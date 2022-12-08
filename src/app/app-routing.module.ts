a0a9d492c756f997232eeaa4dc5f92cbead532db
felipessantana <felipe.emenen@hotmail.com>
2022-12-08 16:18:38 -0300

    build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
+import { DetalhesProdutosComponent } from './produtos/detalhes-produtos/detalhes-produtos.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
+  //Redirecionando direto para tela de produtos
+  {path: "", redirectTo: "produtos", pathMatch: "full"}, 
+  //{path: '', component: ProdutosComponent},
+  { path: 'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) },
+  {path: 'produtos/:id', component: DetalhesProdutosComponent},
+  { path: 'contato', loadChildren: () => import('./contato/contato.module').then(m => m.ContatoModule) },
+  //Redirecionar para pagina que não tem conteudo
+  {path: "**", component: NaoEncontradaComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/carrinho/carrinho.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarrinhoRoutingModule } from './carrinho-routing.module';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    
+  ],
+  imports: [
+    CommonModule,
+    CarrinhoRoutingModule,
+    FormsModule
+  ]
+})
+export class CarrinhoModule { }
